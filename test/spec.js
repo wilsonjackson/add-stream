0f767690412d949408514bab8b4b4d6097872daf
Majid Burney <moocow@euphoricsoup.com>
2015-04-06 13:19:20 -0700

    Update travis version targets

--- test/spec.js
+++ test/spec.js
@@ -40,6 +40,16 @@ describe('add-stream', function () {
 					done();
 				}));
 		});
+
+		it('should emit an end event if the stream is empty', function (done) {
+			var chunks = ['abc', 'def'];
+			emit(chunks)
+				.pipe(addStream(emit([])))
+				.pipe(es.wait(function (err, buffer) {
+					expect(buffer.toString()).to.equal(chunks.join(''));
+					done();
+				}));
+		});
 	});
 
 	describe('object mode', function () {
@@ -54,7 +64,7 @@ describe('add-stream', function () {
 
 		it('should append a stream from a factory function', function (done) {
 			es.readArray([{p: 1}, {p: 2}, {p: 3}])
-				.pipe(addStream.obj(function () {return es.readArray([{p: 4}, {p: 5}, {p: 6}])}))
+				.pipe(addStream.obj(function () {return es.readArray([{p: 4}, {p: 5}, {p: 6}]);}))
 				.pipe(es.writeArray(function (err, array) {
 					expect(array).to.eql([{p: 1}, {p: 2}, {p: 3}, {p: 4}, {p: 5}, {p: 6}]);
 					done();
